2c44c048a820d65365bbecc831ea6ebdaa31ff66
joaobrito <Joao.brito@pge.ce.gov.br>
2025-09-22 15:49:24 -0300

    apenas dando um commit

--- Repetições.java
+++ Repetições.java
@@ -6,35 +6,35 @@ public class Repetições {
 	public static void main(String[] args) {
 	//For(contador;condição;incremento){ocorrencia}
 		
-	int[] Vetor = {1,2,4,8,16,32,64,128,256};
+	int[] vetor = {1,2,4,8,16,32,64,128,256};
 	//"criando" um Vetor
 
     for(int i=0; i<10; i++) {
-    	 System.out.println(Vetor[i]);
+    	 System.out.println(vetor[i]);
     	 //criei um contador chamado i, que vale certo. Sob uma condição:
     	 //enquanto i for menor que 10, ele aumenta +1 unidade (linha: i++)
         }
     //while(apenas a condição){ocorrencia}
     
     
-    int Incrementador = 9;
-    while(Vetor.length < 10) {
-     Vetor = new int[Incrementador];
-     Incrementador++;
+    int incrementador = 9;
+    while(vetor.length < 10) {
+     vetor = new int[incrementador];
+     incrementador++;
     	// While significa, enquanto. Sendo mais simples.
     }
     
  // do while:
  
  
-    int SegundoIncrementador = 0;
+    int segundoincrementador = 0;
 do {
 
-Vetor[9+SegundoIncrementador] = SegundoIncrementador;
+vetor[9+segundoincrementador] = segundoincrementador;
 	
-SegundoIncrementador++;
+segundoincrementador++;
 
-}while(SegundoIncrementador<10);
+}while(segundoincrementador<10);
     
     
     //serve para priorizar o ciclo em vez da condição. A condição é analisado só por ultimo
