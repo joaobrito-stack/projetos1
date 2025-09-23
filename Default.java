e060872be8055b80dacbec54ecf8cf8575f270ad
joaobrito <Joao.brito@pge.ce.gov.br>
2025-09-23 16:58:09 -0300

    defini uma função para a class Default

--- Default.java
+++ Default.java
@@ -1,10 +1,18 @@
 package exercicios;
-
+import java.util.Scanner;
 public class Default {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int potencia=0;
+		int decimal =0;
+		String string = scanner.next();
+		for(int i = string.length()-1; i>=0; i--) {
+			decimal += Math.pow(2,potencia)*Character.getNumericValue(string.charAt(i));
+			potencia++;
+		}
+		System.out.println(decimal);
+		                   
 	}
 
 }
